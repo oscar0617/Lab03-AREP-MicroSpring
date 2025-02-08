ba0a69e068d76d070bcee0b896a0f75937d46fd7
Oscar Santiago Lesmes Parra <oscarsantiagolesmes@gmail.com>
2025-02-07 22:02:04 -0500

    Final

--- src/main/java/edu/escuelaing/arep/clasehoy/Test.java
+++
@@ -1,9 +0,0 @@
-package edu.escuelaing.arep.clasehoy;
-
-import java.lang.annotation.Retention;
-import java.lang.annotation.RetentionPolicy;
-
-@Retention(RetentionPolicy.RUNTIME)
-public @interface Test {
-    
-}
